114bbbc1584a5638b85947d93ed3fe57a67502e6
Gaspard <gaspard.lonchampt@laplateforme.io>
2020-12-15 14:15:49 +0100

    Premiet jet avant Bulma

---
+++ include/discussion.php
@@ -0,0 +1,123 @@
+<?php 
+
+
+try {
+
+$connexion = new PDO("mysql:host=localhost;dbname=discussion", 'root', 'root');
+$connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$requete_admin = $connexion->prepare("SELECT login, date, message FROM messages INNER JOIN utilisateurs ON messages.id_utilisateur = utilisateurs.id ORDER BY messages.id ASC");
+$requete_admin->execute();
+$result = $requete_admin->fetchall(PDO::FETCH_ASSOC);
+setlocale(LC_TIME, ['fr', 'fra', 'fr_FR']);
+}
+    
+catch (PDOException $e) {
+    echo 'Echec de la connexion : ' . $e->getMessage();
+}
+
+if (isset($_POST['comment'])) {
+
+    $comment = htmlspecialchars($_POST['comment']);
+    
+ }
+ 
+ 
+     $error = FALSE;
+     $sucess = FALSE;
+     $sucessMsg = FALSE;
+     $errorMsg = FALSE;
+ 
+ 
+    // On vérifie d'abord que les infos viennent du formulaire
+     if (isset($_POST["validate"])) {
+       // On vérifie que tous les champs sont remplis
+         if ($comment == NULL) {
+             
+             $error = TRUE;
+             $errorMsg = "Veuillez remplir tous les champs !";
+ 
+       }
+ 
+       else {
+ 
+         $requete_commentaire = $connexion->prepare(
+             "INSERT INTO messages(message,id_utilisateur,date)
+             VALUES(:message,:id_utilisateur,:date)"
+          );
+ 
+          $id_utilisateur = $_SESSION['id'];
+          $date = date('Y-m-d H:i:s');
+          
+          if (
+             $requete_commentaire->execute(array(
+             'message' => $comment,
+             'id_utilisateur' => $id_utilisateur,
+             'date' => $date))                   ) 
+         {  
+             $requete_admin->execute();
+             $result = $requete_admin->fetchall(PDO::FETCH_ASSOC);
+             $sucess = TRUE;
+             $sucessMsg = "Message posté avec succès !";
+             
+         }
+ 
+         else {
+             print_r($requete_commentaire->errorInfo());
+         }
+ 
+          $requete_commentaire->closeCursor();
+       }
+ 
+     }
+
+?>
+
+<div class="container p-3">
+<div class="agileits-top">
+
+            <?php 
+            foreach ($result as $key => $value) { ?>
+                <table class="table table-striped table-hover table-dark">
+                <tbody>
+                <tr>
+                    <td scope="col" class="text-left">
+                    <?php echo $value['login'] ?> </td>
+                    <td class="text-right"> 
+                    <?php echo utf8_encode(strftime('Le %A %d %B %Y',strtotime($value['date']))) ?> </td>
+                </tr>
+                <tr>
+                    <td scope="col" colspan="2">
+                    <?php echo $value['message'] ?> </td>
+                </tr>
+               
+            <?php
+             }   
+             ?>
+        </tbody>
+    </table>
+</div>
+</div>
+
+<div class="main-w3layouts wrapper">
+	<div class="main-agileinfo">
+		<div class="agileits-top">
+            <form  class="was-validated" action="../pages/discussion.php" method="post">
+                <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Ecrivez votre commentaire ici" name="comment" required></textarea>
+                <input type="submit" name="validate" value="Valider">
+
+                <?php 
+
+                    if(isset($error, $sucess)){
+                       if($error == TRUE) {
+                       echo '<span> <h5 class="text-danger text-center">'.$errorMsg.'</h5> </span>'; 
+                       }
+                       elseif ($sucess = TRUE) {
+                        echo '<span> <h5 class="text-white text-center">'.$sucessMsg.'</h5> </span>';
+                       }
+                    }
+                    ?> 
+
+			</form>
+        </div>
+    </div>
+</div>
